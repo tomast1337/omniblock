10023105d8a8f7ea361836c9f1e9ab50fc179fa3
TheVeryStarlk <45287860+TheVeryStarlk@users.noreply.github.com>
2026-02-14 05:44:51 +0300

    Add comment

--- BetaSharp.Launcher/Features/New/NewViewModel.cs
+++ BetaSharp.Launcher/Features/New/NewViewModel.cs
@@ -9,6 +9,7 @@ internal sealed partial class NewViewModel(AuthenticationService authenticationS
     [RelayCommand]
     private async Task AuthenticateAsync()
     {
+        // What to do if the browser tab was closed?
         var result = await authenticationService.OwnsMinecraftAsync();
     }
 }
