d137623c6bcf02ceb0903fb545dd15fc90956149
TheVeryStarlk <45287860+TheVeryStarlk@users.noreply.github.com>
2026-02-20 02:49:44 +0300

    Add STJ flag in project file

--- BetaSharp.Launcher/Features/Home/HomeViewModel.cs
+++ BetaSharp.Launcher/Features/Home/HomeViewModel.cs
@@ -9,13 +9,18 @@ namespace BetaSharp.Launcher.Features.Home;
 internal sealed partial class HomeViewModel(AuthenticationService authenticationService, MinecraftService minecraftService, XboxService xboxService) : ObservableObject
 {
     [RelayCommand]
-    private async Task InitializeAsync()
+    private async Task TestAsync()
     {
         string? microsoft = await authenticationService.GetTokenAsync();
 
         if (string.IsNullOrWhiteSpace(microsoft))
         {
             WeakReferenceMessenger.Default.Send(new NavigationMessage(Destination.Authentication));
+            return;
         }
+
+        var xbox = await xboxService.GetAsync(microsoft);
+        string minecraft = await minecraftService.GetTokenAsync(xbox.Token, xbox.Hash);
+        var profile = await minecraftService.GetProfileAsync(minecraft);
     }
 }
