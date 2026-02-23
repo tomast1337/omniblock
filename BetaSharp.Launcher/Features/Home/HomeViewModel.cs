b76fa952dffe649a172c97054b3af27d6c7b9842
TheVeryStarlk <45287860+TheVeryStarlk@users.noreply.github.com>
2026-02-23 03:28:33 +0300

    Update home view's UI

--- BetaSharp.Launcher/Features/Home/HomeViewModel.cs
+++ BetaSharp.Launcher/Features/Home/HomeViewModel.cs
@@ -1,25 +1,48 @@
 ﻿using System;
+using System.Diagnostics;
+using System.IO;
 using System.Threading.Tasks;
 using BetaSharp.Launcher.Features.Accounts;
+using BetaSharp.Launcher.Features.Messages;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using CommunityToolkit.Mvvm.Messaging;
 
 namespace BetaSharp.Launcher.Features.Home;
 
-internal sealed partial class HomeViewModel(AccountsService accountsService) : ObservableObject
+internal sealed partial class HomeViewModel(AccountsService accountsService, ClientService clientService) : ObservableObject
 {
     [ObservableProperty]
-    public partial string Name { get; set; } = "...";
-
-    private Account? _account;
+    public partial Account? Account { get; set; }
 
     [RelayCommand]
     private async Task InitializeAsync()
     {
-        _account = await accountsService.GetAsync();
+        Account = await accountsService.GetAsync();
 
-        ArgumentNullException.ThrowIfNull(_account);
+        ArgumentNullException.ThrowIfNull(Account);
+    }
 
-        Name = _account.Name;
+    [RelayCommand]
+    private async Task PlayAsync()
+    {
+        ArgumentNullException.ThrowIfNull(Account);
+
+        await clientService.DownloadAsync();
+
+        ArgumentException.ThrowIfNullOrWhiteSpace(Account.Token);
+
+        using var process = Process.Start(Path.Combine(AppContext.BaseDirectory, "Client", "BetaSharp.Client"), [Account.Name, Account.Token]);
+
+        ArgumentNullException.ThrowIfNull(process);
+
+        await process.WaitForExitAsync();
+    }
+
+    [RelayCommand]
+    private async Task SignOutAsync()
+    {
+        WeakReferenceMessenger.Default.Send(new NavigationMessage(Destination.Authentication));
+        await accountsService.DeleteAsync();
     }
 }
