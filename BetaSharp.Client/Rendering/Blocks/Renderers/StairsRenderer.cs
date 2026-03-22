5556282d9c8efa87b654b32ac1145b615fe9073c
Nicolas Vyčas Nery <vycasnicolas@gmail.com>
2026-03-22 16:58:15 -0300

    fix: #488 stairs gui orientation (#489)

--- BetaSharp.Client/Rendering/Blocks/Renderers/StairsRenderer.cs
+++ BetaSharp.Client/Rendering/Blocks/Renderers/StairsRenderer.cs
@@ -10,46 +10,14 @@ public class StairsRenderer : IBlockRenderer
         bool hasRendered = false;
         int direction = ctx.World.getBlockMeta(pos.x, pos.y, pos.z);
 
-        if (direction == 0) // Ascending East (Stairs face West)
-        {
-            // Lower step (West half)
-            var lowerCtx = ctx with { OverrideBounds = new Box(0.0F, 0.0F, 0.0F, 0.5F, 0.5F, 1.0F) };
-            hasRendered |= lowerCtx.DrawBlock(block, pos);
 
-            // Upper step (East half)
-            var upperCtx = ctx with { OverrideBounds = new Box(0.5F, 0.0F, 0.0F, 1.0F, 1.0F, 1.0F) };
-            hasRendered |= upperCtx.DrawBlock(block, pos);
-        }
-        else if (direction == 1) // Ascending West (Stairs face East)
-        {
-            // Upper step (West half)
-            var upperCtx = ctx with { OverrideBounds = new Box(0.0F, 0.0F, 0.0F, 0.5F, 1.0F, 1.0F) };
-            hasRendered |= upperCtx.DrawBlock(block, pos);
+        var upperCtx = ctx with { OverrideBounds = new Box(0.0F, 0.0F, 0.0F, 1.0F, 1.0F, 0.5F) };
+        hasRendered |= upperCtx.DrawBlock(block, pos);
 
-            // Lower step (East half)
-            var lowerCtx = ctx with { OverrideBounds = new Box(0.5F, 0.0F, 0.0F, 1.0F, 0.5F, 1.0F) };
-            hasRendered |= lowerCtx.DrawBlock(block, pos);
-        }
-        else if (direction == 2) // Ascending South (Stairs face North)
-        {
-            // Lower step (North half)
-            var lowerCtx = ctx with { OverrideBounds = new Box(0.0F, 0.0F, 0.0F, 1.0F, 0.5F, 0.5F) };
-            hasRendered |= lowerCtx.DrawBlock(block, pos);
+        // Lower step (South half)
+        var lowerCtx = ctx with { OverrideBounds = new Box(0.0F, 0.0F, 0.5F, 1.0F, 0.5F, 1.0F) };
+        hasRendered |= lowerCtx.DrawBlock(block, pos);
 
-            // Upper step (South half)
-            var upperCtx = ctx with { OverrideBounds = new Box(0.0F, 0.0F, 0.5F, 1.0F, 1.0F, 1.0F) };
-            hasRendered |= upperCtx.DrawBlock(block, pos);
-        }
-        else if (direction == 3) // Ascending North (Stairs face South)
-        {
-            // Upper step (North half)
-            var upperCtx = ctx with { OverrideBounds = new Box(0.0F, 0.0F, 0.0F, 1.0F, 1.0F, 0.5F) };
-            hasRendered |= upperCtx.DrawBlock(block, pos);
-
-            // Lower step (South half)
-            var lowerCtx = ctx with { OverrideBounds = new Box(0.0F, 0.0F, 0.5F, 1.0F, 0.5F, 1.0F) };
-            hasRendered |= lowerCtx.DrawBlock(block, pos);
-        }
 
         // Notice: No cleanup required!
         // The original context remains untouched and the sub-contexts just fall out of scope.
