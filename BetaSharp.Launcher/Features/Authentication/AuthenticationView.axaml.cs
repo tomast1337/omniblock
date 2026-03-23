c141e00a04ad92acf3318494baf8a4ad2352f86c
TheVeryStarlk <45287860+TheVeryStarlk@users.noreply.github.com>
2026-03-23 21:42:19 +0300

    Call authentication service initialization

--- BetaSharp.Launcher/Features/Authentication/AuthenticationView.axaml.cs
+++ BetaSharp.Launcher/Features/Authentication/AuthenticationView.axaml.cs
@@ -1,12 +1,34 @@
-﻿using Avalonia.Controls;
+﻿using System;
+using Avalonia;
+using Avalonia.Controls;
+using Microsoft.Extensions.Logging;
 
 namespace BetaSharp.Launcher.Features.Authentication;
 
 internal sealed partial class AuthenticationView : UserControl
 {
-    public AuthenticationView(AuthenticationViewModel viewModel)
+    private readonly ILogger<AuthenticationView> _logger;
+    private readonly AuthenticationViewModel _viewModel;
+
+    public AuthenticationView(ILogger<AuthenticationView> logger, AuthenticationViewModel viewModel)
     {
+        _logger = logger;
+        _viewModel = viewModel;
+
         DataContext = viewModel;
         InitializeComponent();
+    }
+
+    protected override async void OnAttachedToVisualTree(VisualTreeAttachmentEventArgs eventArgs)
+    {
+        try
+        {
+            base.OnAttachedToVisualTree(eventArgs);
+            await _viewModel.InitializeCommand.ExecuteAsync(null);
+        }
+        catch (Exception exception)
+        {
+            _logger.LogError(exception, "An exception occurred in authentication view on visual tree attach");
+        }
     }
 }
