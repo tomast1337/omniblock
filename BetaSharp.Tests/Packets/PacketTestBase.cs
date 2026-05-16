2d09d169c3a08550f02339671bc391a6559b6ce5
millennIumAMbiguity <millenniumambiguity@noreply.localhost>
2026-05-16 02:39:39 +0200

    more packet tests
    tests to verify that the same amount of data is written and read.

---
+++ BetaSharp.Tests/Packets/PacketTestBase.cs
@@ -0,0 +1,11 @@
+﻿using BetaSharp.Network.Packets;
+
+namespace BetaSharp.Tests.Packets;
+
+public abstract class PacketTestBase
+{
+    public static IEnumerable<object[]> PacketIds =>
+        Enum.GetValues(typeof(PacketId))
+            .Cast<PacketId>()
+            .Select(v => new object[] { v });
+}
