0cb7c0c0681042040b7965cb5ae422ec0612a1f5
millennIumAMbiguity <millenniumambiguity@noreply.localhost>
2026-06-28 03:13:58 +0200

    typo

--- BetaSharp.Launcher/Features/Authentication/AuthenticationViewModel.cs
+++ BetaSharp.Launcher/Features/Authentication/AuthenticationViewModel.cs
@@ -89,7 +89,7 @@ internal sealed partial class AuthenticationViewModel(
                 "Uh-oh!",
                 "Try again shortly. If the problem persists, create an issue on GitHub."
                 + Environment.NewLine
-                + "https://git.gay/betasharp-official/betasharp/issuess");
+                + "https://git.gay/betasharp-official/betasharp/issues");
         }
     }
 }
